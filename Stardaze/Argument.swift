e46b5fd5ff9bca9a1e0669360133dc45ad1ff1b1
William Wilson <will@letote.com>
2018-02-21 11:07:20 -0800

    Supports more complex arguments

--- Stardaze/Argument.swift
+++ Stardaze/Argument.swift
@@ -11,7 +11,7 @@
  */
 public struct Argument {
     internal let key: String
-    internal let value: Receiver
+    internal var value: Receiver
 
     private init(key: String, receiver: Receiver) {
         self.key = key
@@ -24,13 +24,21 @@ public struct Argument {
      */
     public init?(key: String, value: [Any]) {
         self.key = key
+        self.value = ""
+        let renamedValue = value
 
         do {
-            let converted = try value.map({ (element) throws -> Receiver in
-                guard let gqlValue = element as? Receiver else {
+            let converted = try renamedValue.map({ (element) throws -> Receiver in
+                if let gqlValue = element as? Receiver {
+                    return gqlValue
+                } else if let list = element as? [Any], let gqlList = try? createGraphQLList(list) {
+                    return gqlList
+                } else if let object = element as? [String: Any],
+                    let gqlObject = try? createGraphQLObject(object) {
+                    return gqlObject
+                } else {
                     throw GraphQLValueError.objectConversionError
                 }
-                return gqlValue
             })
 
             self.value = GraphQLList(converted)
@@ -81,19 +89,88 @@ public struct Argument {
      */
     public init?(key: String, value: [String: Any]) {
         self.key = key
+        self.value = ""
+        var renamedValue = value
         do {
             var receiverDict = [String: Receiver]()
-            for (key, anyValue) in value {
-                guard let gqlValue = anyValue as? Receiver else {
-                    throw GraphQLValueError.listConversionError
+            for (key, anyValue) in renamedValue {
+                if let gqlValue = anyValue as? Receiver {
+                    receiverDict[key] = gqlValue
+                } else if let list = anyValue as? [Any],
+                    let gqlList = try? createGraphQLList(list) {
+                    receiverDict[key] = gqlList
+                } else if let object = anyValue as? [String: Any],
+                    let gqlObject = try? createGraphQLObject(object) {
+                    receiverDict[key] = gqlObject
+                } else {
+                    throw GraphQLValueError.objectConversionError
                 }
-                receiverDict[key] = gqlValue
             }
 
             self.value = GraphQLObject(receiverDict)
         } catch (let e) {
             NSLog(e.localizedDescription)
             return nil
+        }
+    }
+    
+    func createGraphQLObject(_ dict: [String: Any]) throws -> GraphQLObject {
+        do {
+            let receiverList: [(String, Receiver)] = try dict.map { (key, value) in
+                if let receiver = value as? Receiver {
+                    return (key, receiver)
+                } else if let subList = value as? [Any] {
+                    do {
+                        return try (key, createGraphQLList(subList))
+                    } catch (let e) {
+                        throw e
+                    }
+                } else if let dict = value as? [String: Any] {
+                    do {
+                        return try (key, createGraphQLObject(dict))
+                } catch (let e) {
+                    throw e
+                    }
+                } else {
+                    throw GraphQLValueError.objectConversionError
+                }
+            }
+            
+            let receiverDict = Dictionary(receiverList, uniquingKeysWith: { (first, _) in
+                return first
+            })
+        
+            return GraphQLObject(receiverDict)
+        } catch (let e) {
+            throw e
+        }
+    }
+    
+    func createGraphQLList(_ list: [Any]) throws -> GraphQLList {
+        do {
+            let receiverList: [Receiver] = try list.map { (object) in
+                if let receiver = object as? Receiver {
+                    return receiver
+                } else if let subList = object as? [Any] {
+                    do {
+                        return try createGraphQLList(subList)
+                    } catch (let e) {
+                        throw e
+                    }
+                } else if let dict = object as? [String: Any] {
+                    do {
+                        return try createGraphQLObject(dict)
+                    } catch (let e) {
+                        throw e
+                    }
+                } else {
+                    throw GraphQLValueError.listConversionError
+                }
+            }
+            
+            return GraphQLList(receiverList)
+        } catch (let e) {
+            throw e
         }
     }
 
